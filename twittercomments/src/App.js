a187a140cc9aa9a69e7db2f5c0cfc62ed66a03ae
nandanareddivari <150907033+nandanareddivari@users.noreply.github.com>
2023-12-15 20:28:44 +0530

    Testimonials

---
+++ twittercomments/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Twitter from "./Components/Twitter/Twitter";
+
+function App() {
+  return (
+    <div className="App">
+      <Twitter/>
+    </div>
+  );
+}
+
+export default App;
